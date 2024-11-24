a0c0a4a2db56cb8caddf983ac2acd539e18a62a9
dreksen <zakirovdanil55@gmail.com>
2024-11-24 22:03:14 +0300

    update-ranking

--- app/routes/profile/profile.py
+++ app/routes/profile/profile.py
@@ -23,12 +23,23 @@ def profile():
 
 @bp.route("/ranking", methods=["GET"])
 def ranking():
-    """Отображает рейтинг пользователей по количеству решённых задач"""
-    users = db.session.query(User, db.func.count(UserTask.id).label("solved_count"))\
-        .join(UserTask, User.id == UserTask.user_id)\
-        .filter(UserTask.solved == True)\
-        .group_by(User.id)\
-        .order_by(db.desc("solved_count"))\
-        .limit(50).all()  # Показываем только топ 50 пользователей
+    """Отображает таблицу с рейтингом пользователей."""
+    # Получаем всех пользователей
+    users = User.query.all()
 
-    return render_template("ranking.html", users=users)
+    # Для каждого пользователя считаем количество решённых задач
+    user_ranks = []
+    for user in users:
+        solved_tasks_count = UserTask.query.filter_by(user_id=user.id, solved=True).count()
+        total_tasks_count = Task.query.count()
+        user_ranks.append({
+            'username': user.username,
+            'solved': solved_tasks_count,
+            'total': total_tasks_count,
+            'rank': solved_tasks_count / total_tasks_count * 100  # Процент решённых задач
+        })
+
+    # Сортируем пользователей по количеству решённых задач в порядке убывания
+    user_ranks.sort(key=lambda x: x['solved'], reverse=True)
+
+    return render_template("ranking.html", user_ranks=user_ranks)
